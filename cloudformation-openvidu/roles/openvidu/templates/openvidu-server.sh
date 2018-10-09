7f381432fc29c7d249e09a4ef9a16ead2a099108
Stavros Zavrakas <st.zavrakas@gmail.com>
2018-10-09 16:12:57 +0100

    force PUBLIC_HOSTNAME to be domain name

--- cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
+++ cloudformation-openvidu/roles/openvidu/templates/openvidu-server.sh
@@ -8,5 +8,7 @@ PUBLIC_HOSTNAME={{ domain_name }}
 PUBLIC_HOSTNAME=$(curl http://169.254.169.254/latest/meta-data/public-hostname)
 {% endif %}
 
+PUBLIC_HOSTNAME={{ domain_name }}
+
 java -jar -Dopenvidu.secret="{{ openvidusecret }}" -Dopenvidu.recording=true -Dopenvidu.recording.public-access={{ FreeHTTPAccesToRecordingVideos }} -Dserver.ssl.enabled=false -Dopenvidu.publicurl=https://${PUBLIC_HOSTNAME}:{{ openvidu_port }} -Dserver.port=5443 -DMY_UID=$(id -u $USER) /opt/openvidu/openvidu-server.jar
 
